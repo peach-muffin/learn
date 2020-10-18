291de01cb3017d668617826de323bf4dbd4d3ec7
peach-muffin <73070520+peach-muffin@users.noreply.github.com>
2020-10-18 20:57:58 +0530

    Add files via upload

---
+++ myfile.sh
@@ -0,0 +1,7 @@
+Script started on 2020-10-18 20:49:54+05:30 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="120" LINES="30"]
+]0;kakul@Kakul-Windows: /mnt/c/Users/kakul/Documents[01;32mkakul@Kakul-Windows[00m:[01;34m/mnt/c/Users/kakul/Documents[00m$ date +"%c"
+Sun Oct 18 20:50:06 2020
+]0;kakul@Kakul-Windows: /mnt/c/Users/kakul/Documents[01;32mkakul@Kakul-Windows[00m:[01;34m/mnt/c/Users/kakul/Documents[00m$ exit
+exit
+
+Script done on 2020-10-18 20:50:08+05:30 [COMMAND_EXIT_CODE="0"]
