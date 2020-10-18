ca798e2a6dd87a3a3031f4c743554ee138f026e7
peach-muffin <73070520+peach-muffin@users.noreply.github.com>
2020-10-18 21:08:58 +0530

    Add files via upload

---
+++ kakul.sh
@@ -0,0 +1,7 @@
+Script started on 2020-10-18 15:37:27+0000
+]0;root@83bc3e967628: /content[01;32mroot@83bc3e967628[00m:[01;34m/content[00m# date
+Sun Oct 18 15:37:35 UTC 2020
+]0;root@83bc3e967628: /content[01;32mroot@83bc3e967628[00m:[01;34m/content[00m# exit
+exit
+
+Script done on 2020-10-18 15:37:42+0000
